1d22e364ddbf1724e6c7443a1b64a58642f69f03
leonardomastrangelo <leo.mastrangelo.st@gmail.com>
2024-01-08 15:10:00 +0100

    problem with create

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -21,7 +21,7 @@ class ComicController extends Controller
     /**
      * Show the form for creating a new resource.
      *
-     * 
+     * @return \Illuminate\View\View;
      */
     public function create()
     {
